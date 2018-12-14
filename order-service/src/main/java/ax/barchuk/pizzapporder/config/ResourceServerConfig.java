f8c4407e7dd8c7ac8bb90a37855a766763093a4f
Alex Barchuk <ax.barchuk@gmail.com>
2018-12-14 16:43:25 +0200

    Migrated to k8s way of microservice implementation.

--- order-service/src/main/java/ax/barchuk/pizzapporder/config/ResourceServerConfig.java
+++ order-service/src/main/java/ax/barchuk/pizzapporder/config/ResourceServerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.security.oauth2.client.feign.OAuth2FeignRequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
@@ -31,6 +32,14 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Bean
     public OAuth2RestTemplate clientCredentialsRestTemplate() {
         return new OAuth2RestTemplate(clientCredentialsResourceDetails());
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/actuator/**").permitAll()
+                .anyRequest().authenticated()
+                .and().csrf().disable();
     }
 
 }
